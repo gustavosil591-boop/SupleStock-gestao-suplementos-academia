75eb2c82c2f7bee7e8ace3e0058c3dbdbdfcc713
gustavosil591-boop <gustavosil591@gmail.com>
2026-06-28 18:25:01 -0300

    fix: garantindo login.js na pasta front

---
+++ front/login.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB6dz1GREueNHbvlBqDAxOky2OZ89zzLTU",
+    authDomain: "gest-c9436.firebaseapp.com",
+    projectId: "gest-c9436",
+    storageBucket: "gest-c9436.firebasestorage.app",
+    messagingSenderId: "631199034647",
+    appId: "1:631199034647:web:94b7fc0bdf2155cd365b52"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const btnGoogle = document.getElementById('btn-google');
+
+if (btnGoogle) {
+    btnGoogle.addEventListener('click', async () => {
+        try {
+            const resultado = await signInWithPopup(auth, provider);
+            console.log("Usuário logado com sucesso:", resultado.user.displayName);
+            
+            
+            window.location.href = "produtos.html";
+        } catch (erro) {
+            console.error("Erro no login com Google:", erro);
+            alert("Erro ao entrar com o Google. Verifique se ativou o provedor no console do Firebase.");
+        }
+    });
+}

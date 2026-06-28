88004933f5b55adec52500f2a7a821e2ce0d2554
gustavosil591-boop <gustavosil591@gmail.com>
2026-06-28 18:39:45 -0300

    fix: corrigindo maiuscula da apiKey do firebase

--- front/login.js
+++ front/login.js
@@ -6,12 +6,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6dz1GREueNHbvlBqDAxOky2OZ89zzLTU",
+    apiKey: "AIzaSyB6dz1GREueNHbvlBqDAxOkY2OZ89zzLTU",
     authDomain: "gest-c9436.firebaseapp.com",
     projectId: "gest-c9436",
     storageBucket: "gest-c9436.firebasestorage.app",
     messagingSenderId: "631199034647",
-    appId: "1:631199034647:web:94b7fc0bdf2155cd365b52"
+    appId: "1:631199034647:web:94b7fc0bdf2155cd365b52",
+    measurementId: "G-1ZN2TYMV4N"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,11 +27,10 @@ if (btnGoogle) {
             const resultado = await signInWithPopup(auth, provider);
             console.log("Usuário logado com sucesso:", resultado.user.displayName);
             
-            
             window.location.href = "produtos.html";
         } catch (erro) {
             console.error("Erro no login com Google:", erro);
-            alert("Erro ao entrar com o Google. Verifique se ativou o provedor no console do Firebase.");
+            alert("Erro ao entrar com o Google. Verifique o console do navegador.");
         }
     });
 }
